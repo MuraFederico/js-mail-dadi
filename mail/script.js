399ce69c9950adfa0f3f28c279143ac9cea1785a
MuraFederico <federicomura98@gmail.com>
2022-02-24 14:48:10 +0100

    controllo mail funzionante

---
+++ mail/script.js
@@ -0,0 +1,17 @@
+const arrayMails = ['1@mail.com' , '2@mail.com' , '3@mail.com' , '4@mail.com' , '5@mail.com' ,'6@mail.com' , '7@mail.com' , '8@mail.com'];
+
+const userMail = prompt('type your e-mail');
+
+let mailRegistered = false;
+
+for (let i = 0; i < arrayMails.length; i++) {
+    if (userMail.toUpperCase() == arrayMails[i].toUpperCase()){
+        mailRegistered = true;
+    }
+    
+}
+if (mailRegistered == true) {
+    console.log('la mail esiste');
+} else {
+    console.log('la mail non esiste');
+}

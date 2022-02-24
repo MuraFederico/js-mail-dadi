7bcf22f008336205a644343fe8147e8bc9c8493b
MuraFederico <federicomura98@gmail.com>
2022-02-24 15:01:30 +0100

    aggiunto controllo del vincitore

--- dadi/script.js
+++ dadi/script.js
@@ -4,3 +4,10 @@ let computerDie = Math.floor(Math.random() * 6 + 1);
 console.log(playerDie);
 console.log(computerDie);
 
+if (playerDie > computerDie) {
+    console.log('You win!')
+}else if (computerDie > playerDie) {
+    console.log('You lose :c')   
+} else {
+    console.log('pareggio!')
+}

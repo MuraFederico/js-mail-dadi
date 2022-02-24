851bddc14281c9035be8d5f80da9062eeebe311c
MuraFederico <federicomura98@gmail.com>
2022-02-24 15:16:44 +0100

    aggiunta interfaccia primitiva

--- dadi/script.js
+++ dadi/script.js
@@ -1,13 +1,29 @@
-let playerDie = Math.floor(Math.random() * 6 + 1);
-let computerDie = Math.floor(Math.random() * 6 + 1);
+const btnStart = document.querySelector('#start')
+const btnPlayAgain = document.querySelector('#play-again')
 
-console.log(playerDie);
-console.log(computerDie);
+function diceGame(){
 
-if (playerDie > computerDie) {
-    console.log('You win!')
-}else if (computerDie > playerDie) {
-    console.log('You lose :c')   
-} else {
-    console.log('pareggio!')
+    btnStart.classList.remove('show');
+    btnPlayAgain.classList.add('show');
+
+    let playerDie = Math.floor(Math.random() * 6 + 1);
+    let computerDie = Math.floor(Math.random() * 6 + 1);
+    
+    // console.log(playerDie);
+    // console.log(computerDie);
+    
+    if (playerDie > computerDie) {
+        console.log('You win!');
+    }else if (computerDie > playerDie) {
+        console.log('You lose :c') ;  
+    } else {
+        console.log('pareggio!');
+    }
+
+
 }
+
+
+btnStart.addEventListener('click', diceGame);
+
+btnPlayAgain.addEventListener('click', diceGame);
